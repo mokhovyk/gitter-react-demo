a9c004ecb6a68888b5b1baa1a56145f9318560db
Andrii Mokhovyk <andrii.mokhovyk@gmail.com>
2018-06-09 11:19:05 -0500

    Add Async/await fetching

--- client/src/actions/rooms.js
+++ client/src/actions/rooms.js
@@ -3,22 +3,18 @@ import {
   RECEIVE_ROOMS,
 } from './types';
 
+import api from '../helpers/api';
+
 export const receiveRooms = (data) => ({
   type: RECEIVE_ROOMS,
   items: data,
 });
 
-export const fetchRooms = (userId) => (dispatch) => {
-  fetch(`/rooms/?user=${userId}`, {
-    credentials: 'include',
-  })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(data => {
-      dispatch(receiveRooms(data))
-    })
-    .catch(() => {
-      throw Error();
-    });
+export const fetchRooms = (userId) => async (dispatch) => {
+  try {
+    const payload = await api(`/rooms/?user=${userId}`);
+    dispatch(receiveRooms(payload))
+  } catch (error) {
+    throw Error();
+  }
 };
