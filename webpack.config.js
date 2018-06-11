1c7c6392e741166af05e5528e55101f98cd9b618
Andrii Mokhovyk <andrii.mokhovyk@gmail.com>
2018-06-11 22:45:41 +0300

    Change npm run tasks

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: path.join(__dirname, '/client/src/index.js'),
 
   output: {
-    path: path.join(__dirname, '/client/dist/'),
+    path: path.join(__dirname, '/client/build/'),
     filename: 'bundle.js',
   },
 
